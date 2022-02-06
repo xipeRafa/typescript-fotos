8236b0056a5fb79764d6909f70adb1b3deafe622
rafael <rafaelsistem87@gmail.com>
2022-02-06 09:33:04 -0700

    hello

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('hola tontos')
+import express, {Request, Response} from 'express'
+export const app = express()
+
+app.get('/', (req:Request, res:Response) => {
+      res.send('Hola Tontos')
+})
+
+app.listen(3000, ()=> {
+      console.log('servidor en puerto 3000')
+})
